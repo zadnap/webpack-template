358d548b0eb7cea91c9e206bbbadb645202706ef
zadnap <zadnap@users.noreply.github.com>
2025-01-08 23:00:53 +0700

    feat(sass): add sass to webpack framework

--- webpack.common.mjs
+++ webpack.common.mjs
@@ -35,6 +35,10 @@ export default {
         use: ['style-loader', 'css-loader'],
       },
       {
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
       },
